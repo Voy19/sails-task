5fe62fcff554ef8fc1165bec94c07592e8b371f1
Voy19 <denys.voytov@gmail.com>
2019-11-19 20:53:02 +0200

    added changed in assessments controller

--- api/controllers/AssessmentsController.js
+++ api/controllers/AssessmentsController.js
@@ -174,8 +174,8 @@ module.exports = {
                      return {
                         name: reviewer.userId.name,
                         surname: reviewer.userId.surname,
-                        "Interaction with colleagues": reviewer["Interaction with colleagues"],
-                        "Quality of task closure": reviewer["Quality of task closure"],
+                        "IWC": reviewer["Interaction with colleagues"],
+                        "QTC": reviewer["Quality of task closure"],
                         "code quality": reviewer["code quality"],
                         competence: reviewer.competence,
                         discipline: reviewer.discipline,
