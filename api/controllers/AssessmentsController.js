7aa3139d7fdc5c83735dd7372d29e09533735770
Den <denys.voytov@gmail.com>
2019-11-22 13:16:21 +0200

    fixed bug v2

--- api/controllers/AssessmentsController.js
+++ api/controllers/AssessmentsController.js
@@ -173,7 +173,7 @@ module.exports = {
                      return {
                         name: reviewer.userId.name,
                         surname: reviewer.userId.surname,
-                        "IWС": reviewer["Interaction with colleagues"],
+                        "IWC": reviewer["Interaction with colleagues"],
                         "QTC": reviewer["Quality of task closure"],
                         "code quality": reviewer["code quality"],
                         competence: reviewer.competence,
